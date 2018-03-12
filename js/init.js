e4eb9d6160c2ddefe2be21e36482bcfa2da1c99b
Emma Dickson <ejd315@nyu.edu>
2018-03-12 19:58:23 -0400

    portfolio

--- js/init.js
+++ js/init.js
@@ -2,6 +2,6 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
   }); // end of document ready
 })(jQuery); // end of jQuery name space
