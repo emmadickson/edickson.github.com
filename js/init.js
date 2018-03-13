3fb1917b700a9756b401876e03784173fc2cebb6
Emma Dickson <ejd315@nyu.edu>
2018-03-12 20:24:52 -0400

    test

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-     $('.collapsible').collapsible();
+     $('.collapsible').collapsible( accordion : true);
     $('.carousel.carousel-slider').carousel({fullWidth: true});
   }); // end of document ready
 })(jQuery); // end of jQuery name space
