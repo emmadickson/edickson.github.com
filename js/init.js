35fadcafa7d851ab96bd9ed68dd2d9d64eabe2b3
Emma Dickson <ejd315@nyu.edu>
2018-04-03 10:19:27 -0400

    speed

--- js/init.js
+++ js/init.js
@@ -3,6 +3,5 @@
 
     $('.button-collapse').sideNav();
      $('.collapsible').collapsible( {accordion : true});
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
   }); // end of document ready
 })(jQuery); // end of jQuery name space
